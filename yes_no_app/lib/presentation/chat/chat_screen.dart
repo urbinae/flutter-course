73ee628ae15d39a11fe3ede84e7547de2b7a40e8
Eimar <eimar.urbina@gmail.com>
2026-01-08 15:45:05 -0400

    chat

--- yes_no_app/lib/presentation/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -16,6 +16,34 @@ class ChatScreen extends StatelessWidget {
         title: const Text('Mi amor 🥰'),
         //centerTitle: true,
       ),
+      body: _ChatView()
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Hola Mundo $index');
+                },
+              ),
+            ),
+            Text('Hola'),
+        
+          ],
+        ),
+      ),
     );
   }
 }
